00c7bc5d495ebd540f0f8a7989ea4d855b8b1732
Emmadrigal <emma.madri97@gmail.com>
2017-03-23 09:17:34 -0600

    Added base user interface
    Created VGA sync generator
    Added basic options
    
    Image display is missing

---
+++ pixel_Gen.v
@@ -0,0 +1,414 @@
+module pixel_Gen(	
+	//Inputs from the sincronizer
+	input pixel_tick,
+	input wire [9:0] pixel_x, pixel_y,
+	input wire video_on,
+	
+	input wire [1:0] selectImage,
+	
+	//Salida de color segun el bit correspondiente
+    output reg [7:0] red_channel,
+	output reg [7:0] blue_channel,
+	output reg [7:0] green_channel
+   );
+   
+
+
+// constant declaration
+
+localparam SymbolSize = 16 ; //Lenght of the side of the digit
+localparam expansionSize =  1;//Denotes log2(SymbolSize/8) it denotes how much bigger does the digit is on screen compared with the template, 1 is 1:1, 2 is 1:4, 3 is 1:16. From Verilog 2005 and on this can implemented as a function
+	
+
+//Current character to be drawn
+reg [7:0] Character;
+//Used to determine the height of the current pixel in the caracter
+reg [2:0] columnY;
+//Used to read the current row of pixels in the character
+wire [7:0] row;
+
+//Module instatiation
+Chars_rom CharacterRom(.character(Character),  .columnaY(columnY),   .data(row));
+
+//Checks that for a given pixel in the screen if it should be written
+always @(posedge pixel_tick) begin
+	if ((pixel_x >= 24) && (pixel_x <= 120) && (pixel_y >= 24) && (pixel_y <= 40)) begin
+		columnY = (pixel_y - 24) >> expansionSize;
+		if (pixel_x <= 40) begin// 
+			Character = "L";
+			if(row[7-((pixel_x - 24) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 56) begin// 
+			Character = "E";
+			if(row[7-((pixel_x - 40) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 72) begin// 
+			Character = "N";
+			if(row[7-((pixel_x - 56) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 88) begin// 
+			Character = "N";
+			if(row[7-((pixel_x - 72) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 104) begin// 
+			Character = "A";
+			if(row[7-((pixel_x - 88) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else begin// 
+			Character = 95;
+			if(row[7-((pixel_x - 104) >> expansionSize)]   && (selectImage == 0)) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+	end
+	
+	else if ((pixel_x >= 24) && (pixel_x <= 120) && (pixel_y >= 48) && (pixel_y <= 64)) begin
+		columnY = (pixel_y - 48) >> expansionSize;
+		if (pixel_x <= 40) begin// 
+			Character = "B";
+			if(row[7-((pixel_x - 24) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 56) begin// 
+			Character = "O";
+			if(row[7-((pixel_x - 40) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 72) begin// 
+			Character = "A";
+			if(row[7-((pixel_x - 56) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 88) begin// 
+			Character = "T";
+			if(row[7-((pixel_x - 72) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 104) begin// 
+			Character = "S";
+			if(row[7-((pixel_x - 88) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		
+		else begin// 
+			Character = 95;
+			if(row[7-((pixel_x - 104) >> expansionSize)]   && (selectImage == 1)) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+	end
+	
+	else if ((pixel_x >= 24) && (pixel_x <= 152) && (pixel_y >= 72) && (pixel_y <= 88)) begin
+		columnY = (pixel_y - 72) >> expansionSize;
+		if (pixel_x <= 40) begin// 
+			Character = "B";
+			if(row[7-((pixel_x - 24) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 56) begin// 
+			Character = "A";
+			if(row[7-((pixel_x - 40) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 72) begin// 
+			Character = "R";
+			if(row[7-((pixel_x - 56) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 88) begin// 
+			Character = "B";
+			if(row[7-((pixel_x - 72) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 104) begin// 
+			Character = "A";
+			if(row[7-((pixel_x - 88) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 120) begin// 
+			Character = "R";
+			if(row[7-((pixel_x - 104) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 136) begin// 
+			Character = "A";
+			if(row[7-((pixel_x - 120) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else begin// 
+			Character = 95;
+			if(row[7-((pixel_x - 136) >> expansionSize)]   && (selectImage == 2)) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		
+	end
+	
+	else if ((pixel_x >= 24) && (pixel_x <= 136) && (pixel_y >= 96) && (pixel_y <= 112)) begin
+		columnY = (pixel_y - 96) >> expansionSize;
+		if (pixel_x <= 40) begin// 
+			Character = "F";
+			if(row[7-((pixel_x - 24) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 56) begin// 
+			Character = "I";
+			if(row[7-((pixel_x - 40) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 72) begin// 
+			Character = "L";
+			if(row[7-((pixel_x - 56) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 88) begin// 
+			Character = "T";
+			if(row[7-((pixel_x - 72) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 104) begin// 
+			Character = "E";
+			if(row[7-((pixel_x - 88) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else if (pixel_x <= 120) begin// 
+			Character = "R";
+			if(row[7-((pixel_x - 104) >> expansionSize)]) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		else begin// 
+			Character = 95;
+			if(row[7-((pixel_x - 120) >> expansionSize)]   && (selectImage == 3)) begin
+				red_channel = -1;
+				blue_channel = -1;
+				green_channel = -1;
+			end
+			else begin
+				red_channel = 0;
+				blue_channel = 0;
+				green_channel = 0;
+			end
+		end
+		
+	end
+	
+
+	
+	else begin
+		red_channel = 0;
+		blue_channel = 0;
+		green_channel = 0;
+	end
+end 
+
+endmodule
---
+++ top.v
@@ -0,0 +1,80 @@
+module top(
+	input wire clk, //Reloj de 50MHz
+	
+	input wire A,
+	input wire B,
+	
+	input wire left_button,
+	input wire right_button,
+	
+	output wire hsync,
+	output wire vsync,
+	
+	output wire vga_clk,
+	output wire vga_blank_n,
+	
+	output wire [7:0] VGA_R,
+	output wire [7:0] VGA_G,
+	output wire [7:0] VGA_B,
+	
+	output wire LED
+);
+
+wire p_tick;
+wire video_on;
+wire [9:0] pixel_x;
+wire [9:0] pixel_y;
+
+assign vga_clk = p_tick;
+assign vga_blank_n = video_on;
+
+
+reg [1:0] selectImage;
+
+vga_sync Sync(
+  .clk(clk),
+  .hsync(hsync), .vsync(vsync), .video_on(video_on), .p_tick(p_tick),
+  .pixel_x(pixel_x), .pixel_y(pixel_y)
+);
+
+pixel_Gen Pixels(	
+	.pixel_tick(p_tick), .video_on(video_on),
+	.pixel_x(pixel_x), .pixel_y(pixel_y),
+	
+	.selectImage(selectImage),
+	.red_channel(VGA_R),
+	.blue_channel(VGA_B),
+	.green_channel(VGA_G)
+);
+
+assign LED = A && B;
+
+//Usado para el control del Boton_Left
+reg cambio_SolicitadoLeft = 0, cambio_RealizadoLeft = 0;
+wire cambio_FinalizadoLeft;
+assign cambio_FinalizadoLeft = ((cambio_RealizadoLeft & cambio_SolicitadoLeft) |  ((!cambio_RealizadoLeft) & (!cambio_SolicitadoLeft)));
+
+//Usado para el control del Boton_Right
+reg cambio_SolicitadoRight = 0, cambio_RealizadoRight = 0;
+wire cambio_FinalizadoRight;
+assign cambio_FinalizadoRight = ((cambio_RealizadoRight & cambio_SolicitadoRight) |  ((!cambio_RealizadoRight) & (!cambio_SolicitadoRight)));
+
+always @(posedge left_button)
+	cambio_SolicitadoLeft = ~cambio_SolicitadoLeft;
+	
+always @(posedge right_button)
+	cambio_SolicitadoRight = ~cambio_SolicitadoRight;
+
+always @(posedge clk) begin
+	if (!cambio_FinalizadoLeft) begin
+		selectImage = selectImage - 1;
+		cambio_RealizadoLeft = ~cambio_RealizadoLeft;
+	end
+	if (!cambio_FinalizadoRight) begin
+		selectImage = selectImage + 1;
+		cambio_RealizadoRight = ~cambio_RealizadoRight;
+	end
+end
+
+
+endmodule
