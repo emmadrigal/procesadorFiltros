c63c2ac8ab988572b65f5485022fad1a638bba52
Emmadrigal <emma.madri97@gmail.com>
2017-03-23 11:15:22 -0600

    Changed representation to hex
    Changed to OpenCV

--- imageConverstion.py
+++ imageConverstion.py
@@ -1,9 +1,18 @@
-import binascii
+import numpy as np
+import cv2
+
+def weightedAverage(pixel):
+    return 0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2]
 
 filename = raw_input("Please enter filename: ")
-with open(filename, 'rb') as f:
-    content = f.read()
+image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+
+imgHex = ""
+
+for i in range(image.shape[0]):
+    for j in range(image.shape[1]):
+        imgHex = imgHex + hex(image[i, j])[2:]
 
 f = open("hexImage.txt", 'w')
-f.write(binascii.hexlify(content))
+f.write(imgHex)
 f.close
