f1c86583003fff4ed9f39ec49b5a186aa7da4c4b
Emmadrigal <emma.madri97@gmail.com>
2017-03-24 14:55:02 -0600

    Correct output to provide a MIF file
    Added a 1000 words space at the beginning of the memory
    File takes three images and scales them to 450, 400 and 350 according to
    their order
    Adds space at the end for the first stage and final image

--- imageConverstion.py
+++ imageConverstion.py
@@ -1,20 +1,68 @@
 import numpy as np
 import cv2
 
-def weightedAverage(pixel):
-    return 0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2]
+filename = raw_input("Please enter the name of the 1st image: ")
+image1 = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+filename = raw_input("Please enter the name of the 2nd image: ")
+image2 = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+filename = raw_input("Please enter the name of the 3rd image: ")
+image3 = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
 
-filename = raw_input("Please enter filename: ")
-image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+image1 = cv2.resize(image1, (450, 450))
+image2 = cv2.resize(image2, (400, 400))
+image3 = cv2.resize(image3, (350, 350))
 
 
-f = open("hexImage.txt", 'w')
+f = open("hexImage.mif", 'w')
 
-for i in range(image.shape[0]):
-    for j in range(image.shape[1]):
-        f.write(hex(image[i, j])[2:3])
-        f.write("\n")
-        f.write(hex(image[i, j])[3:])
-        f.write("\n")
-        
+f.write("DEPTH = ")
+depth = str(3*image1.shape[0]*image1.shape[1] + image2.shape[0]*image2.shape[1] + image3.shape[0]*image3.shape[1] + 1000)
+f.write(depth)
+f.write(";\n")
+
+f.write("WIDTH = 8;\n")
+f.write("ADDRESS_RADIX = HEX;\n")
+f.write("DATA_RADIX = HEX;\n")
+f.write("CONTENT\n")
+f.write("BEGIN\n\n")
+
+val = 1000
+
+for i in range(image1.shape[0]):
+    for j in range(image1.shape[1]):
+        f.write(hex(val)[2:])
+        f.write(" : ")
+        num = hex(image1[i, j])
+        num = num[2:]
+        num = num.rjust(2, '0')
+        f.write(num)
+        f.write(";\n")
+        val = val + 1
+
+
+for i in range(image2.shape[0]):
+    for j in range(image2.shape[1]):
+        f.write(hex(val)[2:])
+        f.write(" : ")
+        num = hex(image2[i, j])
+        num = num[2:]
+        num = num.rjust(2, '0')
+        f.write(num)
+        f.write(";\n")
+        val = val + 1
+
+for i in range(image3.shape[0]):
+    for j in range(image3.shape[1]):
+        f.write(hex(val)[2:])
+        f.write(" : ")
+        num = hex(image3[i, j])
+        num = num[2:]
+        num = num.rjust(2, '0')
+        f.write(num)
+        f.write(";\n")
+        val = val + 1
+
+f.write("\n")
+f.write("END;\n")
+
 f.close
