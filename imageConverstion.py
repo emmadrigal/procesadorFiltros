ecda5e711e0409df03d4244c973a66237dcd16a1
Emmadrigal <emma.madri97@gmail.com>
2017-03-25 09:31:28 -0600

    Corrected image sizes to fit inside the memory

--- imageConverstion.py
+++ imageConverstion.py
@@ -1,22 +1,29 @@
 import numpy as np
 import cv2
 
+size1 = 400
+size2 = 350
+size3 = 300
+
 filename = raw_input("Please enter the name of the 1st image: ")
 image1 = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+image1 = cv2.resize(image1, (size1, size1))
+
+
 filename = raw_input("Please enter the name of the 2nd image: ")
 image2 = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+image2 = cv2.resize(image2, (size2, size2))
+
+
 filename = raw_input("Please enter the name of the 3rd image: ")
 image3 = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-
-image1 = cv2.resize(image1, (450, 450))
-image2 = cv2.resize(image2, (400, 400))
-image3 = cv2.resize(image3, (350, 350))
+image3 = cv2.resize(image3, (size3, size3))
 
 
-f = open("hexImage.mif", 'w')
+f = open("mem_Init.mif", 'w')
 
 f.write("DEPTH = ")
-depth = str(3*image1.shape[0]*image1.shape[1] + image2.shape[0]*image2.shape[1] + image3.shape[0]*image3.shape[1] + 1000)
+depth = str(size1*size1 + size2*size2 + size3*size3+1000)
 f.write(depth)
 f.write(";\n")
 
@@ -28,8 +35,8 @@ f.write("BEGIN\n\n")
 
 val = 1000
 
-for i in range(image1.shape[0]):
-    for j in range(image1.shape[1]):
+for i in range(size1):
+    for j in range(size1):
         f.write(hex(val)[2:])
         f.write(" : ")
         num = hex(image1[i, j])
@@ -40,8 +47,8 @@ for i in range(image1.shape[0]):
         val = val + 1
 
 
-for i in range(image2.shape[0]):
-    for j in range(image2.shape[1]):
+for i in range(size2):
+    for j in range(size2):
         f.write(hex(val)[2:])
         f.write(" : ")
         num = hex(image2[i, j])
@@ -51,8 +58,8 @@ for i in range(image2.shape[0]):
         f.write(";\n")
         val = val + 1
 
-for i in range(image3.shape[0]):
-    for j in range(image3.shape[1]):
+for i in range(size3):
+    for j in range(size3):
         f.write(hex(val)[2:])
         f.write(" : ")
         num = hex(image3[i, j])
