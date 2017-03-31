e61059ff916f0c848b46ec6732a7efc58c20cd0f
maumonteroj <maumonteroj@gmail.com>
2017-03-31 15:07:19 -0600

    Cambio en la forma de guardar

--- Compilar.py
+++ Compilar.py
@@ -6,10 +6,16 @@ def leer(leer):
     if leer == 1:
         infile = open('datos','r')
         outfile = open('salida','w')
+        contador = 0
         for line in infile:
             solucion = compilar(line)
             solucionenstr = ''.join(str(e) for e in solucion)
-            outfile.write(solucionenstr)
+            print solucionenstr
+            solucionenstr = (format(int(solucionenstr,2), '02x'))
+            print solucionenstr
+            outfile.write('mem_inst['+str(contador)+'] <= 16\'h'+solucionenstr
+            + ';'+'\n')
+            contador = contador + 1
         infile.close()
         outfile.close()
     else:
@@ -21,6 +27,7 @@ def compilar(instruccion):
     instruccion = instruccion.replace(",", " ")
     instruccion = instruccion.replace("  ", " ")
     instruccion = instruccion.split(" ")
+    print instruccion
     operacion = instruccion[0]
     resultado = []
     opcodeytipo = decoopera.decodiopera(operacion)
